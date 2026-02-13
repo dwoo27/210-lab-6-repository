daf8651925eeb15ef6f8d21c9a194d959c1ebd27
dwoo27 <dwooed27@gmail.com>
2026-02-12 19:51:45 -0800

    10 min commit

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -11,15 +11,27 @@ int main()
     const int SIZE = 5; //const for arr size
     double* arr = new double[SIZE]; //dynamic memory allocation for arr
 
-
+    enterArrayData(arr, SIZE);
+    outputArrayData(arr, SIZE);
 }
 
 void enterArrayData(double* arr, int size) {
-    cout << "Data entry for the array: ";
+    cout << "Data entry for the array: " << endl;
+    string buf; 
     
     for (int i = 0; i < size; i++) {
-        cout << "    > Element # " << i << ": " << *(arr + i);
+        cout << "    > Element # " << i << ": ";
+        cin >> buf; *(arr + i) = atof(buf.c_str());
+        cin.ignore(1000, 10);
     }
 
     cout << "Data entry complete.";
+}
+
+void outputArrayData(double* arr, int size) {
+    cout << "Outputting array elements: "; 
+
+    for (int i = 0; i < size; i++) {
+        cout << *(arr + i) << " ";
+    }
 }
