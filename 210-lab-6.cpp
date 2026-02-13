1a969c98a97ce35b33a4cd115e80d04a085103a8
dwoo27 <dwooed27@gmail.com>
2026-02-12 19:42:49 -0800

    10 min commit

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void enterArrayData(double*);
-void outputArrayData(double*);
-double sumArray(double*);
+void enterArrayData(double*, int);
+void outputArrayData(double*, int);
+double sumArray(double*, int);
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int SIZE = 5; //const for arr size
+    double* arr = new double[SIZE]; //dynamic memory allocation for arr
+
+
 }
 
+void enterArrayData(double* arr, int size) {
+    cout << "Data entry for the array: ";
+    
+    for (int i = 0; i < size; i++) {
+        cout << "    > Element # " << i << ": " << *(arr + i);
+    }
+
+    cout << "Data entry complete.";
+}
